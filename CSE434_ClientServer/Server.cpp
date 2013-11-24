120d325752af9744be4f02d4a12955c32943ca7e
Joe Cowen-Richards <joe@richcows.com>
2013-11-24 14:50:36 -0700

    Changed Server.cpp to take void pointers for recvfrom and sendto.

--- CSE434_ClientServer/Server.cpp
+++ CSE434_ClientServer/Server.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
     struct sockaddr_in serverAddr;	/* Local address */
     struct sockaddr_in clientAddr;	/* Client address */
     unsigned int clientAddrLength;  /* Length of incoming message */
-	request newRequest;				/* Request received from client */
+	request *newRequest;				/* Request received from client */
 	vector<clientEntry> clientTable;/* The client table of all the clients */
 	clientEntry newClientEntry;		/* Client for client table lookup */
     unsigned short serverPort;		/* Server port */
@@ -76,45 +76,46 @@ int main(int argc, char* argv[])
         clientAddrLength = sizeof(clientAddr);
 
         /* Block until receive message from a client */
-        if ((recvMsgSize = recvfrom(sock, newRequest, sizeof(newRequest), 0,
+        if ((recvMsgSize = recvfrom(sock, (void*)newRequest, sizeof(newRequest), 0,
 			(struct sockaddr *) &clientAddr, (int*) &clientAddrLength)) < 0)
             DieWithError("recvfrom() failed");
 
         printf("Handling client %s\n", inet_ntoa(clientAddr.sin_addr));
 
 		/* Lookup the client in the client talbe */
-		auto it = find_if(clientTable.begin(), clientTable.end(), find_id(newRequest.client));
+		auto it = find_if(clientTable.begin(), clientTable.end(), find_id(newRequest->client));
 
 		/* If the client is not found, add it */
 		if (it == clientTable.end()) {
-			clientTable.push_back(Client(getpid(), newRequest.inc, newRequest.client, 0));
+			clientTable.push_back(Client(getpid(), newRequest->inc, newRequest->client, 0));
 		}
 		/* Else update the existing entry */
 		else {
 			it->process_id = getpid();
-			it->inc = newRequest.inc;
-			it->client = newRequest.client;
+			it->inc = newRequest->inc;
+			it->client = newRequest->client;
 		}
 
 		/* These cases compare 'R' (client request number) to 'r' (server request number) */
-		if (newRequest.req < it->requestNum) {
+		if (newRequest->req < it->requestNum) {
 			printf("The request from %d was ignored.\n", clientAddr.sin_addr);
-			printf("The client's request number (%d) was less than the server's request number (%d)\n\n", newRequest.req, it->requestNum);
+			printf("The client's request number (%d) was less than the server's request number (%d)\n\n", newRequest->req, it->requestNum);
 		}
-		else if (newRequest.req == it->requestNum) {
+		else if (newRequest->req == it->requestNum) {
 			/* Send received datagram back to the client */
-			if (sendto(sock, it->sendMsg, recvMsgSize, 0, 
+			if (sendto(sock, (void*)&it->sendMsg, recvMsgSize, 0, 
 				 (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != recvMsgSize)
 				DieWithError("sendto() sent a different number of bytes than expected");
-			printf("The client's request number (%d) was equal to the server's request number (%d)\n\n", newRequest.req, it->requestNum);
+			printf("The client's request number (%d) was equal to the server's request number (%d)\n\n", newRequest->req, it->requestNum);
 		}
-		else if (newRequest.req > it->requestNum) {
-			it->sendMsg = ModifyString(it->sendMsg, newRequest.c);
+		else if (newRequest->req > it->requestNum) {
+			/* Modify the message to send back */
+			it->sendMsg = ModifyString(it->sendMsg, newRequest->c);
 			/* Send received datagram back to the client */
-			if (sendto(sock, it->sendMsg, recvMsgSize, 0, 
+			if (sendto(sock, (void*)&it->sendMsg, recvMsgSize, 0, 
 				 (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != recvMsgSize)
 				DieWithError("sendto() sent a different number of bytes than expected");
-			printf("The client's request number (%d) was greater than the server's request number (%d)\n\n", newRequest.req, it->requestNum);
+			printf("The client's request number (%d) was greater than the server's request number (%d)\n\n", newRequest->req, it->requestNum);
 		}
 
 		sort(clientTable.begin(), clientTable.end(), compareByClient);
